4a0346bd5a65257e55d8acd9dc5449c218d3d01e
vyminh0404 <vyminh0404@gmail.com>
2024-10-01 08:42:33 +0000

    new file:   .idx/dev.nix 	modified:   all/word-press/dynamic_rmk.js

--- all/word-press/dynamic_rmk.js
+++ all/word-press/dynamic_rmk.js
@@ -3,22 +3,21 @@ var dr_event_type = "",
 	dr_items = [],
 	businessType = "retail";
 
-var is_item = window.location.href.includes("/san-pham");
-var is_cart = window.location.href.includes("/thanh-toan");
+// var is_item = window.location.href.includes("/san-pham"); // /product
+var is_item = document.querySelector(".product-container");
+var is_cart = window.location.href.includes("/cart"); // /gio-hang
 var is_CV = window.location.href.includes("/order-received");
 
-if (!(is_item || is_CV)) {
-	dataLayer.push({
-		event: "remarketing",
-	});
-} else if (is_cart) {
+if (is_cart) {
+	// ----------------------------------------------
+	// -----  Update Cart  -----
 	var storage = JSON.parse(localStorage.getItem("item_storage")) || [];
-	jQuery('[name*="checkout"]').on("click", function () {
-		var _arr = jQuery(".product-remove a")
-			.map(function () {
-				return +this.getAttribute("data-product_id");
-			})
-			.toArray();
+	jQuery('a[class*="checkout"]').on("click", function () {
+		var _arr = Array.from(document.querySelectorAll(".product-remove a")).map(
+			function (e) {
+				return +e.getAttribute("data-product_id");
+			}
+		);
 		localStorage.setItem(
 			"item_storage",
 			JSON.stringify(
@@ -28,34 +27,33 @@ if (!(is_item || is_CV)) {
 			)
 		);
 	});
+}
+else if (!is_item && !is_CV) {
+	dataLayer.push({
+		event: "remarketing",
+	});
 } else {
 	if (is_item) {
-		// -----------------------
+		// ----------------------------------------------
 		// -----  View Item  -----
 		dr_event_type = "view_item";
-		var itemId = jQuery(".chitiet > li:nth-child(1)").text().split(": ")[1];
-
 		var item = {
-			id: itemId,
-			wpId: +jQuery('[name="add-to-cart"]')[0].value,
+			id: +jQuery('[name*="add-to-cart"]').val(),
 			google_business_vertical: businessType,
 		};
 		dr_items.push(item);
-		dr_value = +jQuery("ins .amount")[0].innerText.replace(/\D/g, "");
+		dr_value = +jQuery(".product-main ins .amount").text().replace(/\D/g, "");
 
-		// -------------------------
+		// ------------------------------------------------
 		// -----  Add To Cart  -----
-		jQuery('[class*="add_to_cart"], [class*="buy_now"]').on(
+		jQuery('button[class*="add_to_cart"], button[class*="buy_now"]').on(
 			"click",
 			function () {
 				dr_event_type = "add_to_cart";
 				var current_cart =
 					JSON.parse(localStorage.getItem("item_storage")) || [];
 				current_cart.push(item);
-				localStorage.setItem(
-					"item_storage",
-					JSON.stringify(current_cart)
-				);
+				localStorage.setItem("item_storage", JSON.stringify(current_cart));
 
 				dataLayer.push({
 					dr_event_type: dr_event_type,
@@ -63,10 +61,9 @@ if (!(is_item || is_CV)) {
 					dr_items: dr_items,
 					event: "dynamic_remarketing",
 				});
-			}
-		);
+			});
 	} else if (is_CV) {
-		// ----------------------
+		// ---------------------------------------------
 		// -----  Purchase  -----
 		dr_event_type = "purchase";
 		var dr_items = JSON.parse(localStorage.getItem("item_storage"));
